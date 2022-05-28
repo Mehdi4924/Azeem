0d6b8ead5c1a58b24fc4711ca844cb021221063d
Mehdi4924 <85990825+Mehdi4924@users.noreply.github.com>
2022-05-28 19:21:20 +0500

    Add files via upload

---
+++ Utilities/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    white : "#ffffff",
+    purple: "#701D75",
+    blue:"#03438B",
+    black:"#000000",
+    success:"#4BB543",
+    danger:"#d9534f",
+    facebook:'#3B5998',
+    google:'#518EF8',
+}
+
+export default colors;
---
+++ Utilities/url.js
@@ -0,0 +1,6 @@
+//// Live Url for Api's
+const url= "https://admin.azeemdigitalsolutions.com/api"
+// const paymenturl= "https://admin.azeemdigitalsolutions.com/"
+//// Development Url For Api'ss
+// const url= "https://azeemebook.ranglerz.pw/api"
+export default url;
